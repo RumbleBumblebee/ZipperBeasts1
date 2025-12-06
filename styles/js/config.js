7e44b78a8e14e489fa43d122be7f7d26d58486a8
RumbleBumblebee <128832140+RumbleBumblebee@users.noreply.github.com>
2025-12-06 14:27:29 -0500

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -17,7 +17,7 @@ let charadex = {};
 charadex.site = {
   title: "ZipperBeasts",
   url: "https://charadex-team.github.io/charadex-v1.0/",
-  description: `A tool for organizing small ARPGs and species.`
+  description: `A placeholder masterlist of all zipperbeasts, items, and traits.`
 }
 
 /* ==================================================================== */
